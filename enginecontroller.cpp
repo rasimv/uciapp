577bd1864c73f0c974b5f2bade71661a53874c30
rasimv <rasimvaliullin@hotmail.com>
2017-08-28 02:27:59 +0300

    Add EngineController class and moc things

---
+++ enginecontroller.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "enginecontroller.h"
+
+EngineController::EngineController(QObject *a)
+{}
+
+EngineController::~EngineController()
+{}
---
+++ enginecontroller.h
@@ -0,0 +1,15 @@
+#ifndef __ENGINECONTROLLER_H
+#define __ENGINECONTROLLER_H
+
+#include <QObject>
+
+class EngineController : public QObject
+{
+    Q_OBJECT
+
+public:
+    EngineController(QObject *a = nullptr);
+    ~EngineController();
+};
+
+#endif //__ENGINECONTROLLER_H
