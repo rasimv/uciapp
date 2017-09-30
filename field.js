5c94a1153be7e86fb5afc6ebc62527d93f8dfdb3
rasimv <rasimvaliullin@hotmail.com>
2017-09-30 03:50:19 +0300

    Forgotten js-file

---
+++ field.js
@@ -0,0 +1,30 @@
+.pragma library
+
+.import "chessutil.js" as ChessUtil
+
+var s_imageFilenames = ["images/Chess_pdg45.svg", "images/Chess_pdg45.svg", "images/Chess_pdg45.svg",
+                        "images/Chess_pdg45.svg", "images/Chess_pdg45.svg", "images/Chess_pdg45.svg",
+                        "images/Chess_pdg45.svg", "images/Chess_pdg45.svg", "images/Chess_pdg45.svg",
+                        "images/Chess_pdg45.svg", "images/Chess_pdg45.svg", "images/Chess_pdg45.svg"];
+
+function imageFilename(a)
+{
+    var l_found = ChessUtil.s_pawnsAndPieces.indexOf(a);
+    return l_found < 0 ? "" : s_imageFilenames[l_found];
+}
+
+function imageQML(a_filename)
+{
+    var s = "import QtQuick 2.7\n" +
+            "import QtQuick.Controls 2.0\n" +
+            "Image\n" +
+            "{\n" +
+                "x: 0; y: 0\n" +
+                "width: parent.width\n" +
+                "height: parent.height\n" +
+                "sourceSize.width: 256\n" +
+                "sourceSize.height: 256\n" +
+                "source: \"%1\"\n" +
+            "}\n";
+    return s.arg(a_filename);
+}
